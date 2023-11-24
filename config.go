eb5868cd5ae92415c617e6d4ac1171a327650a0a
Devlin Hitchcock <developer.dmh@gmail.com>
2023-11-24 09:31:13 +0200

    Add basic yaml config file to store local system specific config

---
+++ config.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	configDir     = ".config/config-cli"
+	gitConfigFile = "git.yaml"
+)
+
+type CLIConfig struct {
+	GitDir   string `yaml:"gitDir"`
+	WorkTree string `yaml:"workTree"`
+}
+
+func LoadConfig() (CLIConfig, error) {
+	var conf CLIConfig
+	usr, err := user.Current()
+	if err != nil {
+		return conf, err
+	}
+
+	fullPath := fmt.Sprint(usr.HomeDir, "/", configDir, "/", gitConfigFile)
+
+	_, err = os.Stat(fullPath)
+	if err != nil {
+		if err := os.MkdirAll(fmt.Sprint(usr.HomeDir, "/", configDir), 0777); err != nil {
+			return conf, err
+		}
+
+		fi, err := os.Create(fullPath)
+		if err != nil {
+			return conf, err
+		}
+
+		contents, err := yaml.Marshal(conf)
+		if err != nil {
+			return conf, err
+		}
+
+		_, err = fi.Write(contents)
+		if err != nil {
+			return conf, err
+		}
+
+		err = fi.Close()
+		if err != nil {
+			return conf, err
+		}
+	}
+
+	fi, err := os.Open(fullPath)
+	if err != nil {
+		return conf, err
+	}
+
+	contents, err := io.ReadAll(fi)
+	if err != nil {
+		return conf, err
+	}
+
+	err = yaml.Unmarshal(contents, &conf)
+	if err != nil {
+		return conf, err
+	}
+
+	return conf, nil
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 var (
@@ -11,5 +12,16 @@ var (
 )
 
 func main() {
-	fmt.Println(fmt.Sprint("Config version: ", Version, ", Build time: ", BuildTime, ", Git hash: ", GitHash))
+	if len(os.Args) == 1 {
+		fmt.Println(fmt.Sprint("Config version: ", Version, ", Build time: ", BuildTime, ", Git hash: ", GitHash))
+		os.Exit(0)
+	}
+
+	conf, err := LoadConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(conf)
 }
